bd69e98ca7504c785f81a7e002cca7592dd79649
Everton Paulouski <paulouskipsk@gmail.com>
2024-07-04 10:17:59 -0300

    Correção atribuição de porta

--- src/app/services/commons/HttpService.ts
+++ src/app/services/commons/HttpService.ts
@@ -15,8 +15,7 @@ export class HttpService {
 
     public token: any;
     private apiAddress: any;
-    private timeout: number = 80000;
-    private msgErroServidor: string = "Erro ao executar requisição com servidor, verifique a conexão com a Internet ou se o servidor está respondendo.";
+    private timeout: number = 30000;
 
     constructor(
         private http: HttpClient, 
@@ -24,9 +23,10 @@ export class HttpService {
         private modalCtrl: ModalController
     ) {
         let apiData = StorageService.getSessionItem('provider-routine');
+        console.log("APIDATA: ", apiData);
         if(apiData){
             this.apiAddress = `${apiData.protocol}://${apiData.ip}`;
-            this.apiAddress += apiData.port ? `:${apiData.port}/api` : '/api';
+            this.apiAddress += (apiData?.port && apiData.port == 80 || apiData.port == 443) ? '/api' : `:${apiData.port}/api`;
             this.timeout = apiData.timeout ? apiData.timeout * 1000 : 80000;
         }else{
             this.utilService.toastInfo('Endereço da API não foi informado. Verifique!', 'danger');
@@ -50,19 +50,11 @@ export class HttpService {
             response = await CapacitorHttp.post(options);
             return response.data;
         } catch (error:any) {
-            throw this.msgErroServidor;
+            throw this. getErrorMessage(error);
         }
     }
 
-    private prepareBody(data: any) {
-        let payload: any;
-        if(data instanceof Map){
-            payload = JSON.stringify(Object.fromEntries(data));
-        }else{
-            payload = data;
-        }
-        return payload;
-    }
+   
 
     public async put(route: string, data: any) {
         try {
@@ -80,7 +72,7 @@ export class HttpService {
             const response: HttpResponse = await CapacitorHttp.put(options);
             return response.data;
         } catch (error) {
-            throw this.msgErroServidor;
+            throw this. getErrorMessage(error);
         }
     }
 
@@ -112,7 +104,7 @@ export class HttpService {
                 });
             });
         } catch (error) {
-            throw this.msgErroServidor;
+            throw this. getErrorMessage(error);
         }
     }
 
@@ -126,6 +118,34 @@ export class HttpService {
         }).then(modal => {
             modal.present();
         })        
+    }
+
+    private prepareBody(data: any) {
+        let payload: any;
+        if(data instanceof Map){
+            payload = JSON.stringify(Object.fromEntries(data));
+        }else{
+            payload = data;
+        }
+        return payload;
+    }
+
+    private getErrorMessage(error: any) {
+        if (error?.code == 'SSLHandshakeException') 
+            return "* Erro ao estabelecer conexão SSL com o servidor remoto.\n\n" + 
+                   "* Este erro pode ser configuração da porta SSL. A porta padrão constuma ser a '445', experimente alterar esta configuração!";
+
+        if (error?.code == 'SocketTimeoutException')
+            return "* Erro ao receber resposta do Servidor. \n"+
+                   "* Tempo de espera Excedido!\n"+
+                   "* Verifique a conexão com a internet\n";
+                   "  ou as configurações de conexão com a API.";
+
+        if (error?.code == 'MalformedURLException')
+            return "* Erro ao processar requisição ao servidor. \n\n"+
+                   "* Verifique o endereço da API e porta nas configurações!";
+
+        return "Erro ao executar requisição com servidor, verifique a conexão com a Internet ou se o servidor está respondendo.";
     }
 
 }
